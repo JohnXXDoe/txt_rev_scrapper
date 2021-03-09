03c54dc353944ddc27df2d60b83eb108380ce037
Utkarsh Kharayat <utkarshkhrt69@gmail.com>
2021-03-09 11:49:16 +0530

    v6.1 Added Try Except to avoid false Page 404 errors Fixed Asin input not taking 1st value

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ from bs4 import BeautifulSoup
 requests.packages.urllib3.disable_warnings(
     requests.packages.urllib3.exceptions.InsecureRequestWarning)  # Suppress insecure warning
 # Email headers
-time_prd = 0    # Time period storage variable
+time_prd = 0  # Time period storage variable
 receiver_email = []  # List for emails to be sent to
 headerss = []  # Store headers variable
 titles = []  # Title of the review
@@ -60,11 +60,11 @@ with open(
 
 curr_dat = dt.today()  # Current system date
 day_diff = timedelta(int(time_prd))
-day_diff_logic = timedelta(int(time_prd)+1)
+day_diff_logic = timedelta(int(time_prd) + 1)
 print("Fetch data from after :", (curr_dat - day_diff).strftime("%d-%b-%Y"))
 
 asin.pop(0)
-asin.pop(1)
+#asin.pop(1)
 print(asin)
 print(asin[1])
 
@@ -75,120 +75,141 @@ def get_data(uid):
     no_pages = 1
     flag = False
     while flag is False:
-        if no_pages % 2 == 0:  # To sleep for avoiding timeouts
-            print(
-                'SLEEPING FOR : ' + str((no_pages / 2) * 10) + ' SECONDS.')
-            tm.sleep(20 * (no_pages / 2))
-        print(' ||| PAGE NUMBER - ' + str(no_pages) + ' |||')
-        hedr = random.choice(headerss)
-        headers = {"User-Agent": hedr,
-                   "Accept-Encoding": "gzip, deflate",
-                   "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT": "1",
-                   "Connection": "close", "Upgrade-Insecure-Requests": "1"}
-        tm.sleep(3)
-        session = requests.Session()
-        session.verify = False
-        retry = Retry(connect=3000, backoff_factor=1)
-        adapter = HTTPAdapter(max_retries=retry)
-        session.mount('http://', adapter)
-        session.mount('https://', adapter)
+        for loading_error in range(10):
+            try:
+                if no_pages % 2 == 0:  # To sleep for avoiding timeouts
+                    print(
+                        'SLEEPING FOR : ' + str((no_pages / 2) * 10) + ' SECONDS.')
+                    tm.sleep(20 * (no_pages / 2))
+                print(' ||| PAGE NUMBER - ' + str(no_pages) + ' |||')
+                hedr = random.choice(headerss)
+                headers = {"User-Agent": hedr,
+                           "Accept-Encoding": "gzip, deflate",
+                           "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT": "1",
+                           "Connection": "close", "Upgrade-Insecure-Requests": "1"}
+                tm.sleep(3)
+                session = requests.Session()
+                session.verify = False
+                retry = Retry(connect=3000, backoff_factor=1)
+                adapter = HTTPAdapter(max_retries=retry)
+                session.mount('http://', adapter)
+                session.mount('https://', adapter)
+                r = session.get('https://www.amazon.in/product-reviews/' + str(
+                    uid) + '?ie=UTF8&reviewerType=all_reviews&sortBy=recent&pageNumber=' + str(no_pages),
+                                # verify='./amzn_certi.cer',
+                                headers=headers)  # , proxies=proxies) : ASIN example - B01L7C4IU2
+                print('https://www.amazon.in/product-reviews/' + str(
+                    uid) + '?ie=UTF8&reviewerType=all_reviews&sortBy=recent&pageNumber=' + str(no_pages))
+                content = r.content
+                soup = BeautifulSoup(content)
+                name_p = soup.find('a',
+                                   attrs={'class': 'a-link-normal',
+                                          'data-hook': 'product-link'}, href=True)
+                container = soup.find('div', attrs={'id': 'cm_cr-review_list',
+                                                    'class': 'a-section a-spacing-none review-views celwidget'})  # Whole review container
 
-        r = session.get('https://www.amazon.in/product-reviews/' + str(
-            uid) + '?ie=UTF8&reviewerType=all_reviews&sortBy=recent&pageNumber=' + str(no_pages),
-                        # verify='./amzn_certi.cer',
-                        headers=headers)  # , proxies=proxies) : ASIN example - B01L7C4IU2
-        print('https://www.amazon.in/product-reviews/' + str(
-            uid) + '?ie=UTF8&reviewerType=all_reviews&sortBy=recent&pageNumber=' + str(no_pages))
-        content = r.content
-        soup = BeautifulSoup(content)
+                if name_p and container is not None:
+                    name = name_p.text.strip()  # fetch name of product
+                    '''
+                    p_names.append(name)
+                    uids.append(uid)
+                    titles.append(' ')
+                    ratings.append(' ')
+                    reviews.append(' ')
+                    dates.append(' ')
+                    c_names.append(' ')
+                    url.append(' ')
+                    likes.append(' ')
+                    
+                    '''
 
-        name_p = soup.find('a',
-                           attrs={'class': 'a-link-normal',
-                                  'data-hook': 'product-link'}, href=True)
-        container = soup.find('div', attrs={'id': 'cm_cr-review_list',
-                                            'class': 'a-section a-spacing-none review-views celwidget'})  # Whole review container
-        if name_p and container is not None:
-            name = name_p.text.strip()  # fetch name of product
-            '''
-            p_names.append(name)
-            uids.append(uid)
-            titles.append(' ')
-            ratings.append(' ')
-            reviews.append(' ')
-            dates.append(' ')
-            c_names.append(' ')
-            url.append(' ')
-            likes.append(' ')
-            
-            '''
+                    print('================== Getting data for ' + name + ' ==================')
+                    tm.sleep(3)
 
-            print('================== Getting data for ' + name + ' ==================')
-            tm.sleep(3)
+                    if container is not None:
+                        for r in container.findAll('div',
+                                                   attrs={'data-hook': 'review', 'class': 'a-section review aok-relative'}):
+                            dte_t = r.find('span', attrs={'class': 'a-size-base a-color-secondary review-date',
+                                                          'data-hook': 'review-date'}).text.strip()  # extract text and strip of whitespaces
+                            # ex : Reviewed in India on 28 December 2020
+                            temp = dte_t.split(' ')
+                            dte = str(temp[4]) + ' ' + str(temp[5]) + ' ' + str(temp[6])
+                            # print(formatted)
+                            q_date = dt.strptime(dte, '%d %B %Y')  # Convert string to Date
+                            if (curr_dat - day_diff_logic) <= q_date:
+                                # 7 <= 7
+                                t_title = r.find('a', attrs={'data-hook': "review-title"}, href=True)
+                                title = t_title.find('span').text.strip()
+                                cus = r.find('span', attrs={'class': 'a-profile-name'}).text.strip()
+                                text_div = r.find('div', attrs={'class': 'a-row a-spacing-small review-data'})
+                                review = text_div.find('span').text.strip()
+                                like = r.find('span', attrs={'class': 'a-size-base a-color-tertiary cr-vote-text',
+                                                             'data-hook': 'helpful-vote-statement'})
+                                if like is not None:
+                                    temp1 = like.text.strip()
+                                    temp = temp1.split(' ')
+                                    if temp[0] == 'One':
+                                        likes.append(1)
+                                    else:
+                                        likes.append(temp[0])
+                                else:
+                                    likes.append(0)
 
-            if container is not None:
-                for r in container.findAll('div',
-                                           attrs={'data-hook': 'review', 'class': 'a-section review aok-relative'}):
-                    dte_t = r.find('span', attrs={'class': 'a-size-base a-color-secondary review-date',
-                                                  'data-hook': 'review-date'}).text.strip()  # extract text and strip of whitespaces
-                    # ex : Reviewed in India on 28 December 2020
-                    temp = dte_t.split(' ')
-                    dte = str(temp[4]) + ' ' + str(temp[5]) + ' ' + str(temp[6])
-                    # print(formatted)
-                    q_date = dt.strptime(dte, '%d %B %Y')  # Convert string to Date
-                    if (curr_dat - day_diff_logic) <= q_date:
-                        # 7 <= 7
-                        t_title = r.find('a', attrs={'data-hook': "review-title"}, href=True)
-                        title = t_title.find('span').text.strip()
-                        cus = r.find('span', attrs={'class': 'a-profile-name'}).text.strip()
-                        text_div = r.find('div', attrs={'class': 'a-row a-spacing-small review-data'})
-                        review = text_div.find('span').text.strip()
-                        like = r.find('span', attrs={'class': 'a-size-base a-color-tertiary cr-vote-text',
-                                                     'data-hook': 'helpful-vote-statement'})
-                        if like is not None:
-                            temp1 = like.text.strip()
-                            temp = temp1.split(' ')
-                            if temp[0] == ('One'):
-                                likes.append(1)
-                            else:
-                                likes.append(temp[0])
-                        else:
-                            likes.append(0)
+                                que_lnk = r.find('a', attrs={
+                                    'class': 'a-size-base a-link-normal review-title a-color-base review-title-content a-text-bold',
+                                    'data-hook': 'review-title'}, href=True)
+                                link = que_lnk['href']
+                                rating = r.find('span', attrs={'class': 'a-icon-alt'}).text.strip()
+                                clean = rating.split(' ')
 
-                        que_lnk = r.find('a', attrs={
-                            'class': 'a-size-base a-link-normal review-title a-color-base review-title-content a-text-bold',
-                            'data-hook': 'review-title'}, href=True)
-                        link = que_lnk['href']
-                        rating = r.find('span', attrs={'class': 'a-icon-alt'}).text.strip()
-                        clean = rating.split(' ')
+                                print('================== Getting data for ' + cus + ' ==================')
+                                titles.append(title)
+                                ratings.append(clean[0])
+                                reviews.append(review)
+                                dates.append(q_date.strftime("%d %b %Y"))
+                                c_names.append(cus)
+                                url.append('https://www.amazon.in' + link)
+                                uids.append(uid)  # uids.append(' ')
+                                p_names.append(name)  # p_names.append(' ')
 
-                        print('================== Getting data for ' + cus + ' ==================')
-                        titles.append(title)
-                        ratings.append(clean[0])
-                        reviews.append(review)
-                        dates.append(q_date.strftime("%d %b %Y"))
-                        c_names.append(cus)
-                        url.append('https://www.amazon.in' + link)
-                        uids.append(uid)  # uids.append(' ')
-                        p_names.append(name)  # p_names.append(' ')
+                                print('Rating : ' + rating)
 
-                        print('Rating : ' + rating)
-
-                    elif q_date < (curr_dat - day_diff_logic):
-                        print('------------------ Date Expired ------------------')
+                            elif q_date < (curr_dat - day_diff_logic):
+                                print('------------------ Date Expired ------------------')
+                                return uids, p_names, c_names, dates, titles, ratings, reviews, likes, url
+                    else:
+                        print(name)
+                        likes.append(' ')
+                        c_names.append('100')
+                        titles.append(' ')
+                        ratings.append(' ')
+                        reviews.append(' ')
+                        dates.append(' ')
+                        url.append(' ')
+                        uids.append(' ')  # uids.append(' ')
+                        p_names.append(' ')  # p_names.append(' ')
+                        print("================ NO REVIEWS ================")
                         return uids, p_names, c_names, dates, titles, ratings, reviews, likes, url
-            else:
-                print(name)
-                likes.append(' ')
-                c_names.append('100')
-                titles.append(' ')
-                ratings.append(' ')
-                reviews.append(' ')
-                dates.append(' ')
-                url.append(' ')
-                uids.append(' ')  # uids.append(' ')
-                p_names.append(' ')  # p_names.append(' ')
-                print("================ NO REVIEWS ================")
-                return uids, p_names, c_names, dates, titles, ratings, reviews, likes, url
+
+                    # --------------------------------------- PAGE CONCEPT HERE ---------------------------------------
+                    tm.sleep(1)
+                    if soup.find('li', attrs={'class': 'a-disabled a-last'}):
+                        flag = True
+                        print('//////END OF PAGES///////')
+                        return uids, p_names, c_names, dates, titles, ratings, reviews, likes, url
+                    tm.sleep(1)
+                    if soup.find('li', attrs={'class': 'a-last'}):  # Check if there are more pages?
+                        no_pages += 1
+                    else:
+                        print('SINGLE PAGE ONLY')
+                        return uids, p_names, c_names, dates, titles, ratings, reviews, likes, url
+                else:
+                    raise Exception('| | | | | | PAGE LOAD ERROR / DOES NOT EXIST | | | | | |')
+            except Exception:
+                print('| | | | | | PAGE LOAD ERROR / DOES NOT EXIST | | | | | |')
+                print('| | | | | | RETRYING ATTEMPT NUMBER : ' + str(loading_error) +' OF 10 | | | | | |')
+                tm.sleep(15)
+                continue
 
         else:
             print("************** PAGE DOES NOT EXIST **************")
@@ -205,20 +226,6 @@ def get_data(uid):
             uids.append(uid)
             p_names.append('NA')
             return uids, p_names, c_names, dates, titles, ratings, reviews, likes, url
-
-        # --------------------------------------- PAGE CONCEPT HERE ---------------------------------------
-        tm.sleep(1)
-        if soup.find('li', attrs={'class': 'a-disabled a-last'}):
-            flag = True
-            print('//////END OF PAGES///////')
-            return uids, p_names, c_names, dates, titles, ratings, reviews, likes, url
-        tm.sleep(1)
-        if soup.find('li', attrs={'class': 'a-last'}):  # Check if there are more pages?
-            no_pages += 1
-        else:
-            print('SINGLE PAGE ONLY')
-            return uids, p_names, c_names, dates, titles, ratings, reviews, likes, url
-
 
 def send_mail(receiver, count_rev, count_rat, count_que):
     sender_email = 'utkarsh.kharayat@havells.com'
@@ -340,19 +347,22 @@ def exception_mail(receiver):
 def total_n():
     count_rev, count_rat, count_que = 0, 0, 0
     with open(
-            'C:/Users/33669/PycharmProjects/Outputs/Scraper/reviews.csv', errors="ignore") as csv_file:  # Read headers for avoiding IP timeout
+            'C:/Users/33669/PycharmProjects/Outputs/Scraper/reviews.csv',
+            errors="ignore") as csv_file:  # Read headers for avoiding IP timeout
         reader = csv.reader(csv_file, delimiter=',')
         next(reader)  # Skip header row
         for col in reader:
             count_rev += 1
 
-    with open('C:/Users/33669/PycharmProjects/Outputs/Scraper/ratings.csv', errors="ignore") as csv_file:  # Read Asin values from the csv
+    with open('C:/Users/33669/PycharmProjects/Outputs/Scraper/ratings.csv',
+              errors="ignore") as csv_file:  # Read Asin values from the csv
         reader = csv.reader(csv_file, delimiter=',')
         next(reader)  # Skip header row
         for col in reader:
             count_rat += 1
 
-    with open('C:/Users/33669/PycharmProjects/Outputs/Scraper/questions.csv', errors="ignore") as csv_file:  # Read Asin values from the csv
+    with open('C:/Users/33669/PycharmProjects/Outputs/Scraper/questions.csv',
+              errors="ignore") as csv_file:  # Read Asin values from the csv
         reader = csv.reader(csv_file, delimiter=',')
         next(reader)  # Skip header row
         for col in reader:
@@ -362,11 +372,10 @@ def total_n():
 
 
 try:
-
     for t in range(1, len(asin) - 1):  # len(asin) - 1
         if t % 10 == 0:
-            print('SLEEPING FOR : ' + str(3) + ' Seconds')
-            tm.sleep(3)
+            print('SLEEPING FOR : ' + str(0) + ' Seconds')
+            tm.sleep(0)
         print('==== REVIEWS ====|||||||| PRODUCT NO : ' + str(t) + ' ||||||||')
 
         unique, product_name, cus_name, datess, titless, ratingss, reviewss, likess, urls = get_data(asin[t])
